9bad246091d78f24401dccbc7b99a09d383d781e
Martin Gergov <martingergov1@gmail.com>
2018-05-23 12:22:22 +0200

    selectable hexagons

--- hex.js
+++ hex.js
@@ -6,20 +6,24 @@ function handleMouseDown(e, offsetX, offsetY, ctx, bounding){
     var mouseX=parseInt(e.clientX-offsetX);
     var mouseY=parseInt(e.clientY-offsetY);
     console.log("x:" + mouseX + ";y:" + mouseY);
-    dot_within(mouseX, mouseY, bounding);
+    console.log(dot_within(mouseX, mouseY, bounding));
 }
 
 function hexagon(ctx, x, y, size, fill){
+    var coord = [
+	[x, y], [x+size,y], [x+1.6*size,y+0.8*size], 
+	[x+size,y+1.6*size], [x, y+1.6*size], [x-0.6*size,y+0.8*size]]
     ctx.fillStyle = fill;
     ctx.beginPath();
-    ctx.moveTo(x, y); 		          
-    ctx.lineTo(x+size,y);	          
-    ctx.lineTo(x+1.6*size,y+0.8*size);
-    ctx.lineTo(x+size,y+1.6*size);
-    ctx.lineTo(x, y+1.6*size);	  
-    ctx.lineTo(x-0.6*size,y+0.8*size);
+    ctx.moveTo(coord[0][0], coord[0][1]);
+    ctx.lineTo(coord[1][0], coord[1][1]);
+    ctx.lineTo(coord[2][0], coord[2][1]);
+    ctx.lineTo(coord[3][0], coord[3][1]);
+    ctx.lineTo(coord[4][0], coord[4][1]);
+    ctx.lineTo(coord[5][0], coord[5][1]);
     ctx.closePath();
     ctx.fill();
+    return coord
 }
 
 
@@ -43,13 +47,23 @@ function inside(point, vs) {
     return inside;
 };
 
-function dot_within(x, y, board) {
-    console.log(board)
+function dot_within(x, y, b) {
+    var board = b[0];
+    var hexagons = b[1];
     var bcoords = [[board[0], board[1]],
 		   [board[2], board[3]],
 		   [board[4], board[5]],
 		   [board[6], board[7]]];
-    console.log(inside([x, y], bcoords));
+    if (!inside([x, y], bcoords)){
+	return -1;
+    }
+    for (var i = 0; i < hexagons.length; i++){
+	if (inside([x,y], hexagons[i])){
+	    return i;
+	}
+    }
+    return -1;
+    
 }
 
 function paragram(ctx, coord){
@@ -63,10 +77,12 @@ function paragram(ctx, coord){
     ctx.stroke();
 }
 
-function hexagon_field(ctx, fill, w, h, mask){
+function hexagon_field(ctx, fill, w, h){
+    hexagon_coords = [];
     for (var k = 0; k < 11; k++){
 	for (var i = 0; i < 11; i++){
-	    hexagon(ctx, 10+k*25, 150+18*i-k*10, 10, fill);
+	    coord = hexagon(ctx, 10+k*25, 150+18*i-k*10, 10, fill);
+	    hexagon_coords.push(coord);
 	}
     }
     var bounding = [10-0.6*10, // x1 - upper left
@@ -79,7 +95,7 @@ function hexagon_field(ctx, fill, w, h, mask){
 		    150+18*11+5];  
     console.log("bounding rect: " + bounding);
     paragram(ctx, bounding); 	// board hit box
-    return bounding;    
+    return [bounding, hexagon_coords];
 }
 
 function main() {
@@ -103,7 +119,7 @@ function main() {
     var offsetY=canvasOffset.top;
     var scrollX=canvas.scrollLeft;
     var scrollY=canvas.scrollTop;
-    var b = hexagon_field(ctx, "#000", 640, 480, occupied);
+    var b = hexagon_field(ctx, "#000", 640, 480);
     document.getElementById('glCanvas').onmousedown = function(e){
 	handleMouseDown(e, offsetX, offsetY, ctx, b);
     };
